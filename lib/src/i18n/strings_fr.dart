cb8c22d9feff2fa68a47c93f8c1c19c04b45d03e
Bouali Mohammed <mohamedbouali963@gmail.com>
2019-11-14 02:31:10 +0100

    Update strings_fr.dart

--- lib/src/i18n/strings_fr.dart
+++ lib/src/i18n/strings_fr.dart
@@ -17,18 +17,47 @@ class _StringsFR extends _StringsI18n {
   @override
   List<String> getMonths() {
     // TODO return the array of month [January, February ... December]
-    return [Janvier, Février, Mars, Avril, Mai, Juin, Juillet, Août, Septembre, Octobre, Novembre, Décembre];
+    return [
+      "Janvier", 
+      "Février", 
+      "Mars", 
+      "Avril", 
+      "Mai", 
+      "Juin", 
+      "Juillet", 
+      "Août", 
+      "Septembre", 
+      "Octobre", 
+      "Novembre", 
+      "Décembre"
+    ];
   }
 
   @override
   List<String> getWeeksFull() {
     // TODO return the array of week's full name [Monday, Tuesday ... Sunday]
-    return [Lundi, Mardi, Mercredi, Jeudi, Vendredi, Samedi, Dimanche];
+    return [
+      "Lundi", 
+      "Mardi", 
+      "Mercredi", 
+      "Jeudi", 
+      "Vendredi", 
+      "Samedi", 
+      "Dimanche"
+    ];
   }
 
   @override
   List<String> getWeeksShort() {
     // TODO return the array of week's short name [Mon, Tue ... Sun]
-    return [lun, mar, mer, jeu, ven, sam, dim];
+    return [
+      "lun", 
+      "mar", 
+      "mer", 
+      "jeu", 
+      "ven",
+      "sam",
+      "dim"
+    ];
   }
 }
